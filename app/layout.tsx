acd8400601374826f486acc875fb4e143738549c
HkJayesh <jayeshrathod@HK-MacBook-Air.local>
2026-02-03 22:46:29 +0530

    add the cuelinks java

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,13 +103,14 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              var cId = "262474";
+              var cId =  "262474";
+
               (function(d, t) {
                 var s = document.createElement("script");
                 s.type = "text/javascript";
                 s.async = true;
-                s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/") + "cuelinksv2.js";
-                document.body.appendChild(s);
+                s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/")  + "cuelinksv2.js";
+                document.getElementsByTagName("body")[0].appendChild(s);
               }());
             `,
           }}
