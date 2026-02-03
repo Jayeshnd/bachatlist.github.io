b40f102f6aaa3d993d675251d42cd8bdd095c898
HkJayesh <jayeshrathod@HK-MacBook-Air.local>
2026-02-03 22:39:42 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,11 +86,25 @@ export default function RootLayout({
                 "@type": "SearchAction",
                 target: {
                   "@type": "EntryPoint",
-                  urlTemplate: "https://bachatlist.com/deals?search={search_term_string}",
+                  urlTemplate: "https://www.bachatlist.com/deals?search={search_term_string}",
                 },
                 "query-input": "required name=search_term_string",
               },
             }),
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var cId = "262474";
+              (function(d, t) {
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/") + "cuelinksv2.js";
+                document.getElementsByTagName("body")[0].appendChild(s);
+              }());
+            `,
           }}
         />
       </head>
